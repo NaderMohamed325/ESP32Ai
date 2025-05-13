4753c83760775b9efc78525283d53874f2f2c6b4
Nader Mohamed Nader <nadoorclasher@gmail.com>
2025-05-13 20:08:29 +0300

    Update ml_model.js

--- Esp32/ml_model.js
+++ Esp32/ml_model.js
@@ -2,13 +2,21 @@ const axios = require("axios");
 
 async function predictHealthStatus(hr, spo2) {
   try {
-    const response = await axios.post("https://patient-luck-production.up.railway.app", {
-      heartRate: hr,
-      spo2: spo2,
-    });
+    const response = await axios.post(
+      "https://patient-luck-production.up.railway.app/predict",
+      {
+        heartRate: hr,
+        spo2: spo2,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return response.data.status;
   } catch (e) {
-    console.error("ML server error:", e.message);
+    console.error("ML server error:", e.response?.data || e.message);
     return "Error";
   }
 }
